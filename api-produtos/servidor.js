6a3cbe733ded74b8b4e0a3ac18153c8b59f18b8e
CAMILA JESUS SANTANA <u25317@academico.cotuca.unicamp.br>
2025-09-16 15:51:18 -0300

    Atualização arquivo servidor.js

--- api-produtos/servidor.js
+++ api-produtos/servidor.js
@@ -1,1 +1,35 @@
-console.log('foi')
+require('dotenv').config();
+const express = require("express")
+const app = express();
+const porta = process.env.PORTA;
+const stringSQL = process.env.CONNECTION_STRING;
+const mssql = require('mssql');
+const cors = require('cors');
+
+app.use(cors({
+    origin: "*"
+}));
+
+app.use(express.json());
+
+
+async function conectaBD(){
+    try{
+        await mssql.connect(stringSQL);
+    }
+    catch(error){
+        console.log("Erro na conexão com o banco de dados", error)
+    }
+}
+conectaBD()
+
+app.get("/produtos", async (req, res) => {
+    const produtos = await mssql.query `select * from daroca.produtos`
+    console.log(produtos);
+    res.json(produtos);
+})
+
+//rota principal
+app.listen(porta, () => console.log('API funcionando!'))
+
+app.use('/', (req, res) => res.json({mensagem: 'Servidor em execução'}))
